790f9de64c6626f580feacdd075cbabbbeb98b6d
opet <opet@Academico.edu>
2018-04-13 21:29:35 -0300

    Finalizado Verificacao Palindromo

--- Source/exercicios/desafios/desafios.js
+++ Source/exercicios/desafios/desafios.js
@@ -1,17 +1,22 @@
 function reverseString(str) {
     var res = "";
 
-    for (let i = str.length-1; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         res += str[i]
     };
     return res;
 }
 
 function isPalindrome(str) {
-    var res = reverseString(str) + ":";
-
-    for (let i = 0; i < str.length; i++) {
-        res += str[i];
+    var newSTR = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newSTR += str[i];
     }
-    return res;
+    if (newSTR == str) {
+        console.log("É um palídromo!");
+    } else {
+        console.log("Não é um palídromo!");
+    };
+
+    return newSTR + " <-> " + str;
 }
