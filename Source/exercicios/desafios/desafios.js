1824099f9ac8af66685d5f22b546edf16458c162
opet <opet@Academico.edu>
2018-04-16 22:16:22 -0300

    Atualizados desafios

--- Source/exercicios/desafios/desafios.js
+++ Source/exercicios/desafios/desafios.js
@@ -7,7 +7,7 @@ function reverseString(str) {
     return res;
 }
 
-function isPalindrome(str) {
+/*function isPalindrome(str) {
     var newSTR = "";
     for (let i = str.length - 1; i >= 0; i--) {
         newSTR += str[i];
@@ -19,4 +19,46 @@ function isPalindrome(str) {
     };
 
     return newSTR + " <-> " + str;
+}*/
+
+function isPalindrome(str) {
+    if (reverseString(str) === str) {
+    console.log(reverseString(str) + "<->" + str)
+    console.log ("É um palíndromo");
+    } else {
+        console.log("Não é um palíndromo");
+    }
+}
+
+function fizzBuzz() {
+    for (var i = 1; i <= 100; i++) {
+        var valor = "";
+
+        if (i % 3 === 0) valor += "fizz";
+        if (i % 5 === 0) valor += "buzz";
+        console.log(valor || i);
+    }
+}
+
+function maxCharacters(str) {
+    var guardarValor = {};
+
+    for (var i = 0; i < str.length; i++) {
+        if (!guardarValor[str[i]])
+            guardarValor[str[i]] = 1;
+        else
+            guardarValor[str[i]]++;
+    }
+
+    var charRecorrente = "",
+        numeroVezes = 0;
+
+    for (var key in guardarValor) {
+        if (guardarValor[key] >= numeroVezes) {
+            charRecorrente = key;
+            numeroVezes = guardarValor[key];
+        }
+
+    }
+    console.log(charRecorrente + " aparece " + numeroVezes);
 }
